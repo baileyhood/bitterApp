21b21dfc571270671eb397833060e07b2794dd79
William Hood <williamhood@Williams-MacBook-Pro-2.local>
2016-03-17 06:41:46 -0400

    worked on styling

--- bundle.js
+++ bundle.js
@@ -13454,9 +13454,8 @@ return jQuery;
 },{}],10:[function(require,module,exports){
 module.exports = {
 bitterTmpl: [
-'<article>',
-    '<h2 class= "user"> <%=user%> </h2>',
-    "<h3 class = 'date' > <%= date %></h3>",
+    '<h4 class= "user">   <%=user%> | </h2>',
+    "<h4 class = 'date' > <%= date %></h3>",
     "<div class = 'post-container'>",
         '<p class = "post-message" > <%= postMessage %> </p>',
     "</div>",
@@ -13464,27 +13463,25 @@ bitterTmpl: [
     '<button type="button" class = "delete-button btn btn-default" name="deleteButton">Delete</button>',
     '<button type="button" class = "edit-button btn btn-default" name="editButton">Edit</button>',
     "</div>",
-'</article>',
 ].join(""),
 
 addMessage: [
-  '<div class = "add-message-container">',
+  '<h1> Bitter </h1>',
   '<form class = "">',
   '<input class = "user-input form-control" type="text" placeholder="Enter Username" value="">',
   '<input class = "date-input form-control" type="text" placeholder="Enter Date" value="">',
   '<input class = "post-input form-control" type="text" placeholder="Enter Post" value="">',
   '<button class = "submit-post-button btn btn-default" value ="Submit">Submit</button>',
   '</form>',
-  '</div>'
 ].join(""),
 
 editMessage: [
-  '<div class = "edit-message-container">',
+  '<div class = "edit-message-container inactive">',
   '<form class = "">',
-  '<input class = "user-edit form-control" type="text" placeholder="Enter Username" value="">',
-  '<input class = "date-edit form-control" type="text" placeholder="Enter Date" value="">',
-  '<input class = "post-edit form-control" type="text" placeholder="Enter Post" value="">',
-  '<button class = "submit-edit-button btn btn-default" value ="Submit">Submit</button>',
+  '<input class = "user-edit form-control" type="text" placeholder="" value="<%=user%>">',
+  '<input class = "date-edit form-control" type="text" placeholder="Enter Date" value="<%= date %>">',
+  '<input class = "post-edit form-control" type="text" placeholder="Enter Post" value="<%= postMessage %>">',
+  '<button class = "submit-edit-button btn btn-default" value ="Submit">Submit Edit</button>',
   '</form>',
   '</div>'
 ].join("")
