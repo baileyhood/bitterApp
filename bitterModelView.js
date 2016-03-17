271db50ac7171229f40e4b114fd99f1acf69708f
William Hood <williamhood@Williams-MacBook-Pro-2.local>
2016-03-16 20:41:33 -0400

    working on delete

--- bitterModelView.js
+++ bitterModelView.js
@@ -6,17 +6,26 @@ module.exports = Backbone.View.extend ({
   tagName: 'article',
   template: _.template(tmpl.bitterTmpl),
   editTemplate: _.template(tmpl.editMessage),
-  initialize: function () {},
+  initialize: function () {
+    this.listenTo(this.collection, 'update', this.addAll);
+    this.listenTo(this.collection, 'change', this.addAll);
+  },
   render: function(){
       var markup = this.template(this.model.toJSON());
       this.$el.html(markup);
       return this;
   },
   events: {
-    'click edit-button': 'deletePost',
     'click .edit-button' : 'toggleEdit',
+    'click .delete-button' : 'deletePost'
   },
   toggleEdit: function () {
     this.$el.append(this.editTemplate(this.model.toJSON())).toggleClass();
+  },
+
+  deletePost: function () {
+    console.log("Delete button is being pressed.");
+    this.model.destroy();
   }
+
 });
--- main.js
+++ main.js
@@ -8,8 +8,6 @@ var $ = require('jquery');
 $(document).ready(function () {
   var bitterCol = new BitterCollection();
   bitterCol.fetch().then(function (data) {
-       console.log(bitterCol.models.length, "data", data);
-       // data is ready
        var collection = new BitterCollection(data);
        console.log("collection", collection);
        var postMarkUp = new BitterCollectionView({collection: collection});
